14f589e294111ef95a2f5e7a052179bfb3114c27
tony <bitnpc@gmail.com>
2023-09-24 18:35:51 +0800

    Initial Commit

---
+++ Smth/SmthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmthApp.swift
+//  Smth
+//
+//  Created by Tony Clark on 2023/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SmthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
